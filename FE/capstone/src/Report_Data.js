fbc5eee8d1c4ad9273831d92fd77f8e620d5baa2
Ubuntu <ubuntu@ip-172-31-5-42.ap-northeast-2.compute.internal>
2022-12-14 18:34:49 +0000

    프론트 끝

--- FE/capstone/src/Report_Data.js
+++ FE/capstone/src/Report_Data.js
@@ -1,13 +1,7 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
 import { Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
 import BasicTable from './BasicTable';
-
+import "./index.css";
 
 export default function Report_Data(data) {
 //console.log(data.report);
@@ -17,8 +11,8 @@ export default function Report_Data(data) {
     Array.from(Object.keys(data.report)).map((e,i)=>(
         <Menu>
             <SubMenu label={e} >
-            <MenuItem style={{height: "100%"}}>
-                <BasicTable data={data.report[e]}></BasicTable>
+            <MenuItem style={{height: "100%"}} id="item">
+                <BasicTable data={data.report[e]} idx={e}></BasicTable>
             </MenuItem>
         </SubMenu>
 
